27d12f0aa4f8985741a770490239b632ed2a9a05
Paul Dunlop <stormlrd@gmail.com>
2024-11-11 00:43:20 +1300

    updates

--- scripts/header_script.js
+++ scripts/header_script.js
@@ -2,14 +2,31 @@
 //    "<center><img src=\"images/AWSBUILDERSKIWI.png\"></center><text class=\"heading_subtitle\">NZs Premiere Tech Connection Hub! Link up with Presenters and Members today!</text>",
 
 const headers = [
-  "<img src='images/presenter-banners/1.png' class=\"presenter-banner\">",
   "<br /><h1 class=\"hb\">! Mi Casa, Su Casa !<br>Free discord server for your remote presentations.</h1>",
-    "<br /><h1 class=\"hb\">Connect with the community on Discord today</h1>",
-    "<img src='images/presenter-banners/1.png' class=\"presenter-banner\">",
-    "<img src='images/presenter-banners/2.png' class=\"presenter-banner\">",
-    "<img src='images/presenter-banners/3.png' class=\"presenter-banner\">",
-    "<img src='images/presenter-banners/5.png' class=\"presenter-banner\">",
-    "<center><img src=\"images/AWSBUILDERSKIWI.png\"></center><text class=\"heading_subtitle\">NZs Premiere Tech Connection Hub! Link up with Presenters and Members today!</text>",
+  `<table width=100%><tr bgcolor=white><Td>YOU AMAZING VOLUNTEER COMMUNITY PRESENTERS</Td></tr></table><div class=\"bannerrow\">
+  <div class=\"presenterbannercolumn\">
+    <a href=\"https://dunlop.geek.nz\"><img src=\"images/presenter-banners/1.png\" style=\"width:100%;\"></a>
+  </div>
+  <div class=\"presenterbannercolumn\">
+    <img src=\"images/presenter-banners/2.png\" style=\"width:100%;\">
+  </div>
+  <div class=\"presenterbannercolumn\">
+    <img src=\"images/presenter-banners/3.png\" style=\"width:100%;\">
+  </div>
+</div>`,
+"<br /><h1 class=\"hb\">Connect with the community on Discord today</h1>",
+`<table width=100%><tr bgcolor=white><Td>YOU AMAZING VOLUNTEER COMMUNITY PRESENTERS</Td></tr></table><div class=\"bannerrow\">
+<div class=\"presenterbannercolumn\">
+  <img src=\"images/presenter-banners/4.png\" style=\"width:100%;\">
+</div>
+<div class=\"presenterbannercolumn\">
+  <img src=\"images/presenter-banners/5.png\" style=\"width:100%;\">
+</div>
+<div class=\"presenterbannercolumn\">
+  
+</div>
+</div>`,
+  "<center><img src=\"images/AWSBUILDERSKIWI.png\"></center><text class=\"heading_subtitle\">NZs Premiere Tech Connection Hub! Link up with Presenters and Members today!</text>",
 ];
   
   let currentIndex = 0;
@@ -29,5 +46,5 @@ const headers = [
   }
   
   // Change header every 3 seconds
-  setInterval(changeHeader, 7000);
+  setInterval(changeHeader, 5000);
   
